d0dc6ee4d37845dd7f9f31af0b50e93c8f4430bd
Sumit chouhan <sumitkhan.dhana@gmail.com>
2024-09-23 10:48:38 +0530

    add icons

---
+++ src/app/components/common/Icons.jsx
@@ -0,0 +1,18 @@
+export const toggle = () => {
+  return (
+    <svg
+      width="16"
+      height="14"
+      viewBox="0 0 16 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 13H15M1 1H15H1ZM1 7H15H1Z"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
