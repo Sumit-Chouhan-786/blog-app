91abc4fbc1234a356fc1e16f1f628265508ffb2c
SunilVerma0358 <sunilverma93060@gmail.com>
2024-09-23 10:51:31 +0530

    first

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Login from "../components/Login";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      {" "}
+      <Login />{" "}
+    </>
+  );
+};
 
-export default page
+export default page;
